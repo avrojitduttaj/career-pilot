72be6ad9e656bcad78ad07b4fc1342176e96f6e3
avrojitduttaj <avrojitduttaj@gmail.com>
2026-06-05 17:26:40 +0530

    add: Readme Generator

---
+++ frontend/src/components/github/ReadmeGenerator.jsx
@@ -0,0 +1,3 @@
+export default function ReadmeGenerator() {
+  return <div className="text-white p-10">README Generator Working</div>;
+}
---
+++ frontend/src/pages/ReadmeGeneratorPage.jsx
@@ -0,0 +1,7 @@
+export default function ReadmeGeneratorPage() {
+  return (
+    <div style={{ color: "white", padding: "40px", fontSize: "30px" }}>
+      TEST PAGE WORKING
+    </div>
+  );
+}
